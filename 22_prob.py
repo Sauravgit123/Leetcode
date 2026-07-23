8f5c7fbceb08896347b22c464d053223f234a6cf
Saurav <saurav316kumar@gmail.com>
2026-07-23 22:35:24 +0530

    leetcode_ques+

--- 22_prob.py
+++ 22_prob.py
@@ -19,12 +19,24 @@
 # 1 <= n <= 8
 
 
-def solve(i,temp):
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
 
+        def backtrack(s, open, close):
+            if len(s) == 2 * n:
+                ans.append(s)
+                return
 
+            if open < n:
+                backtrack(s + "(", open + 1, close)
 
+            if close < open:
+                backtrack(s + ")", open, close + 1)
 
+        backtrack("", 0, 0)
+        return ans
 
-
-
-n=3
+# Time: O(Cₙ × n) (Catalan number)
+# Auxiliary Space: O(n)
+# Total Space (including output): O(Cₙ × n)
